2a3b94e8bc7f81aad739043ad1589e6a3bb31b36
priyanshu5179 <priyanshuyadav876585.in>
2025-09-14 21:55:49 +0530

    multiplication of two number

--- 1c++.cpp
+++ 1c++.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main() {
     int a = 5, b = 6;
-    int sum = a + b;
-    cout << "The sum of two values is: " << sum << endl;
+    //int sum = a + b;
+    int multiply = (a*b);
+    //cout << "The sum of two values is: " << sum << endl;
+    cout << "multiplication of two number:" << multiply << endl;
     return 0;
 }
