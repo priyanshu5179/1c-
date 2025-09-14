728ecaf78c8affc376992f7a1a7cc221b556a794
priyanshu5179 <priyanshuyadav876585.in>
2025-09-14 21:43:28 +0530

    sum of two num

--- 1c++.cpp
+++ 1c++.cpp
@@ -1,2 +1,9 @@
-This is my first code c++
-this aurthor priyanshu
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 6;
+    int sum = a + b;
+    cout << "The sum of two values is: " << sum << endl;
+    return 0;
+}
